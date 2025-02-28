e15c1e1eb1a82d1c3356178522c44ece94e3f0dc
rflihmmm <indomilk04@gmail.com>
2025-03-01 00:16:00 +0800

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue"
+
+const count = ref(1)
+
+function double() {
+  return (count.value *= 2)
+}
+</script>
+
+<template>
+  <div>
+    <button @click="double()" class="text-xl">counter is : {{ count }}</button>
+  </div>
+</template>
