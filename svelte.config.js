02cfa4083a85f6e11cbd2614c148add464991244
rflihmmm <indomilk04@gmail.com>
2025-05-04 03:18:17 +0800

    update again

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,13 @@ import adapter from '@sveltejs/adapter-static'
 
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build'
-    }),
+    adapter: adapter(),
     alias: {
       '@/*': './path/to/lib/*'
     },
-    paths: { base: process.env.VITE_BASE_PATH || '' }
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
   }
 }
 
